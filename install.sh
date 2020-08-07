cf6e86e06a823687312f045985b47e7c5e179e87
bo <xixixhalu@gmail.com>
2020-08-07 15:07:13 -0700

    update install.sh commands

--- install.sh
+++ install.sh
@@ -1,11 +1,9 @@
 mkdir -pv input result
 
-sudo pip install requests
 pip3 install nltk
 pip3 install requests
 pip3 install pandas
 pip3 install openpyxl
 pip3 install tqdm
 pip3 install xlrd
-
 pip3 install pygtrie

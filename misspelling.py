ad03c55d426bd287d59d3c701b68d203e02fefe7
bo <xixixhalu@gmail.com>
2021-03-12 14:10:05 -0800

    update misspelling.py and generate output

--- misspelling.py
+++ misspelling.py
@@ -208,6 +208,7 @@ def correctFile(file_origin_lines, file_preprocess_lines, correct_dict, file_fre
         for sentence in file_preprocess_lines[idx]:
             correct_line = sentence
             for word in correct_dict:
+                # Bo: temporary matching pattern, may not suit for all cases.
                 pattern = '(\W|^)' + word + 's{0,1}(\W)'
                 if re.search(pattern, correct_line): # case sensitive to avoid change of Proper noun
                     correct_line = re.sub(pattern, rf'\1{correct_dict[word]}\2', correct_line)

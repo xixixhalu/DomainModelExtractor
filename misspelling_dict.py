00db7c8e8c5602ec85fceabbd20b2e4ca8ff6867
Wenhuang <huangwen@usc.edu>
2021-04-23 00:06:56 -0700

    refine technical glossary

--- misspelling_dict.py
+++ misspelling_dict.py
@@ -17,29 +17,42 @@ def word_count(files):
 
 def word_freq_count(wordCount, n):
 	# output words with frequency n 
-	res = []
+	ret = []
 	for word, freq in wordCount:
 		if freq == n:
-			res.append(word)
-	return res
+			ret.append(word)
+	return ret
+
+def read_glossary(file):
+	# output a list of strings in glossary file  
+	with open (file, "r") as myfile:
+			ret = myfile.read().lower().split('\n')
+	return ret
 
 def main():
-    DATA_DIR = "./Data/input_origin"
-    DATA_FILES = glob.glob(DATA_DIR + "/[0-9]*-USC-Project[0-9]*.txt")
-    # DATA_DIR = "./output/misspelling_detect_1"
-    # DATA_FILES = glob.glob(DATA_DIR + "/[0-9]*-USC-Project[0-9]*corrected.txt")
+    # DATA_DIR = "./Data/input_origin"
+    # DATA_FILES = glob.glob(DATA_DIR + "/[0-9]*-USC-Project[0-9]*.txt")
+    DATA_DIR = "./output/misspelling_detect_1"
+    DATA_FILES = glob.glob(DATA_DIR + "/[0-9]*-USC-Project[0-9]*corrected.txt")
     wordCount = word_count(DATA_FILES)
     wordFreq = word_freq_count(wordCount, 1)
     print("#words appears once: %d " %len(wordFreq))
     spell = SpellChecker()
+    
+    # load glossary 
+    glossary = read_glossary("./output/misspelling_detect_1/glossary/glossary.txt")
+    spell.word_frequency.load_words(glossary)
+
     misspelled = spell.unknown(wordFreq)
     print("#words misspelled: %d " %len(misspelled))
     res = list(misspelled)
+    print(res)
+    # write results 
+    
     OUTPUT_DIR = "./output/misspelling_detect_1/glossary/"
     with open(OUTPUT_DIR + "dictionary.txt", "w") as f:
     	for word in res:
     		f.write('%s\n' % word)
-	    
-	    	
+
 if __name__ == "__main__":
     main()

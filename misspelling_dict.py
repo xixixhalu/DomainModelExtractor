d2cc6ba566f90ef8940f316852dab5ff15cc825a
Andrewli97 <y.li9798@hotmail.com>
2021-06-21 09:49:21 +0800

    misspelling detection

--- misspelling_dict.py
+++ misspelling_dict.py
@@ -5,18 +5,19 @@ from spellchecker import SpellChecker
 import re
 
 def word_count(files):
-	# output word count on txt files without punctuation 
+	# output word count on txt files without punctuation
 	data = str()
 	for file in files:
 		with open (file, "r") as myfile:
 			s = myfile.read().replace('\n', ' ').lower()
 			data += s.translate(str.maketrans('', '', string.punctuation))
+			
 	data = data.split(' ')
 	fdist1 = nltk.FreqDist(data)
 	return fdist1.most_common()
 
 def word_freq_count(wordCount, n):
-	# output words with frequency n 
+	# output words with frequency n
 	ret = []
 	for word, freq in wordCount:
 		if freq == n:
@@ -24,7 +25,7 @@ def word_freq_count(wordCount, n):
 	return ret
 
 def read_glossary(file):
-	# output a list of strings in glossary file  
+	# output a list of strings in glossary file
 	with open (file, "r") as myfile:
 			ret = myfile.read().lower().split('\n')
 	return ret
@@ -39,8 +40,8 @@ def main():
     wordFreq = word_freq_count(wordCount, 1)
     print("#words appears once: %d " %len(wordFreq))
     spell = SpellChecker()
-    
-    # load glossary 
+
+    # load glossary
     glossary = read_glossary("./Glossary/glossary.txt")
     spell.word_frequency.load_words(glossary)
 
@@ -48,8 +49,8 @@ def main():
     print("#words misspelled: %d " %len(misspelled))
     res = list(misspelled)
     print(res)
-    # write results 
-    
+    # write results
+
     OUTPUT_DIR = "./Glossary/"
     with open(OUTPUT_DIR + "output.txt", "w") as f:
     	for word in res:

4819b6f34ed48f86cf0d76dba4e5077b497d70d4
bo <xixixhalu@gmail.com>
2021-08-04 18:36:55 -0700

    fix issue of aggregation relationshipin special_TR.py

--- TR/special_TR.py
+++ TR/special_TR.py
@@ -88,14 +88,17 @@ def S_TR2(domain):
         return word_map
         
     root = TrieNode('')
-    # add(root, 'Player')
-    # add(root, 'PlayerData')
-    # add(root, 'PlayerValue')
-    # add(root, 'Admin')
-    # add(root, 'AdminFileOne')
-    # add(root, 'AdminFileTwo')
-    # add(root, 'AdminFile')
-    # add(root, 'PlayerAddressTwo')
+
+    # add_word(root, 'Player')
+    # add_word(root, 'PlayerData')
+    # add_word(root, 'PlayerValue')
+    # add_word(root, 'AdminValue')
+    # add_word(root, 'AdminData')    
+    # add_word(root, 'Admin')
+    # add_word(root, 'AdminFileOne')
+    # add_word(root, 'AdminFileTwo')
+    # add_word(root, 'AdminFile')
+    # add_word(root, 'PlayerAddressTwo')
     # add_word(root, 'Data')
     # add_word(root, 'AdminData')
     # print(TrieNodeEncoder().encode(root))
@@ -104,7 +107,6 @@ def S_TR2(domain):
     relation_list = domain.get_relation()
     entity_dict = domain.get_entity()
     behavior_list = domain.get_behavior()
-    # print(behavior_list.to)
     entities = set(entity_dict.keys())
     entities.update(item.actor for item in behavior_list)
     entities.update(item.target for item in behavior_list)
@@ -112,17 +114,17 @@ def S_TR2(domain):
     entities.update(item.dest for item in relation_list)
     for e in entities:
         add_word(root, e)
-
+    # print(TrieNodeEncoder().encode(root))
     
     new_map = reconstruct_word(root, '', {})
-    # print(new_map)
+    # print(new_map, len(new_map))
     # print(entities)
     
     def transform_helper(word_map, prefix, entities):
         key = list(word_map.keys())[0]
         if key not in entities:
             domain.add_entity(entity_name=key, entity_type='shared')
-            domain.add_relation(source=prefix, dest=key, ass_type='aggregation')
+        domain.add_relation(source=prefix, dest=key, ass_type='aggregation')
 
         if not word_map[key]:
             return (prefix + key, key)
